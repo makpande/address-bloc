a698446bb0c375b346f8723116b79afd97800fa6
Makarand Pande <mak_pande@yahoo.com>
2015-09-09 14:27:36 -0400

    Second commit

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,38 +1,138 @@
-require_relative '../models/address_book'
+require_relative '../models/address_book.rb'
 
 RSpec.describe AddressBook do
-  describe "attributes" do
-    it "should respond to entries" do
-      book = AddressBook.new
-      expect(book).to respond_to(:entries)
-    end
 
-    it "should initialize entries as an array" do
-      book = AddressBook.new
-      expect(book.entries).to be_a(Array)
-    end
+	let(:book)  { AddressBook.new }
 
-    it "should initialize entries as empty" do
-      book = AddressBook.new
-      expect(book.entries.size).to eq(0)
-    end
-  end
+	def check_entry(entry, expected_name, expected_number, expected_email)
+		expect(entry.name).to eql expected_name
+		expect(entry.phone_number).to eql expected_number
+		expect(entry.email).to eql expected_email
+	end
 
-  describe "#add_entry" do
-    it "adds only one entry to the address book" do
-      book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      expec(book.entries.size).to eq(1)
-    end
+	context "attributes" do
+		it "should respond to entries" do
+			expect(book).to respond_to(:entries)
+		end
 
-    it "adds the correct information to entries" do
-      book = AddressBook.new
-      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-      new_entry = book.entries[0]
 
-      expect(new_entry.name).to eq('Ada Lovelace')
-      expect(new_entry.phone_number).to eq('010.012.1815')
-      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+		it "should initialize entries as an array" do
+			expect(book.entries).to be_a(Array)
+		end
+
+		it "should initialize entries as empty" do
+			expect(book.entries.size).to eq 0
+		end
+	end
+	context "#add_entry" do
+		it "adds only one entry to the address book" do
+			book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+			expect(book.entries.size).to eq 1
+		end
+
+		it "adds the correct information to entries" do
+			book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+			new_entry = book.entries[0]
+
+			expect(new_entry.name).to eq 'Ada Lovelace'
+			expect(new_entry.phone_number).to eq '010.012.1815'
+			expect(new_entry.email).to eq 'augusta.king@lovelace.com'
+		end
+	end
+	context ".import_from_csv" do
+
+		before do
+			book.import_from_csv("spec/entries.csv")
+		end
+		it "imports the correct number of entries" do
+
+
+			book_size = book.entries.size
+
+			expect(book_size).to eql 5
+		end
+
+
+		it "imports the 1st entry" do
+			# book.import_from_csv("entries.csv")
+
+			entry_one = book.entries[0]
+
+			check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+		end
+		it "imports the 2nd entry" do
+			# book.import_from_csv("entries.csv")
+
+			entry_two = book.entries[1]
+			check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+		end
+
+		it "imports the 3rd entry" do
+			# book.import_from_csv("entries.csv")
+
+			entry_three = book.entries[2]
+			check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+		end
+
+		it "imports the 4th entry" do
+			# book.import_from_csv("entries.csv")
+
+			entry_four = book.entries[3]
+			check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+		end
+
+		it "imports the 5th entry" do
+			# book.import_from_csv("entries.csv")
+
+			entry_five = book.entries[4]
+			check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+		end
+
+	end
+	def check_entry(entry, expected_name, expected_number, expected_email)
+		expect(entry.name).to eql expected_name
+		expect(entry.phone_number).to eql expected_number
+		expect(entry.email).to eql expected_email
+	end
+
+	context "#binary_search" do
+		before do
+			book.import_from_csv("spec/entries.csv")
+		end
+		it "searches AddressBook for a non-existent entry" do
+			entry = book.binary_search("Dan")
+			expect(entry).to be_nil
+		end
+
+		it "searches AddressBook for Bill" do
+			entry = book.binary_search("Bill")
+			expect(entry).to be_a Entry
+		end
+		it "searches AddressBook for Bob" do
+			entry = book.binary_search("Bob")
+			expect(entry).to be_a Entry
+		end
+
+		it "searches AddressBook for Joe" do
+			entry = book.binary_search("Joe")
+			expect(entry).to be_a Entry
+		end
+
+		it "searches AddressBook for Sally" do
+			entry = book.binary_search("Sally")
+			expect(entry).to be_a Entry
+		end
+
+		it "searches AddressBook for Sussie" do
+			entry = book.binary_search("Sussie")
+			expect(entry).to be_a Entry
+		end
+
+		it "searches AddressBook for Billy" do
+       entry = book.binary_search("Billy")
+       expect(entry).to be_nil
     end
-  end
+	end
+
 end
