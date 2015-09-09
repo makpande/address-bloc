6c3201b29f6a5b9a6df01846b29c5444b7bc57c5
Makarand Pande <mak_pande@yahoo.com>
2015-09-09 15:47:51 -0400

    fourth commit

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -59,34 +59,34 @@ RSpec.describe AddressBook do
 
 			entry_one = book.entries[0]
 
-			check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+			check_entry(entry_one, "Gary", "555-555-4854", "Gary@gmailmail.com")
 		end
 		it "imports the 2nd entry" do
 			# book.import_from_csv("entries.csv")
 
 			entry_two = book.entries[1]
-			check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+			check_entry(entry_two, "Philip", "555-555-5415", "Philip@gmailmail.com")
 		end
 
 		it "imports the 3rd entry" do
 			# book.import_from_csv("entries.csv")
 
 			entry_three = book.entries[2]
-			check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+			check_entry(entry_three, "Arun", "555-555-3660", "Arun@blocmail.com")
 		end
 
 		it "imports the 4th entry" do
 			# book.import_from_csv("entries.csv")
 
 			entry_four = book.entries[3]
-			check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+			check_entry(entry_four, "Mike", "555-555-4646", "Mike@blocmail.com")
 		end
 
 		it "imports the 5th entry" do
 			# book.import_from_csv("entries.csv")
 
 			entry_five = book.entries[4]
-			check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+			check_entry(entry_five, "Sharon", "555-555-2036", "Sharon@blocmail.com")
 		end
 
 	end
@@ -101,7 +101,7 @@ RSpec.describe AddressBook do
 			book.import_from_csv("spec/entries.csv")
 		end
 		it "searches AddressBook for a non-existent entry" do
-			entry = book.binary_search("Dan")
+			entry = book.binary_search("Mack")
 			expect(entry).to be_nil
 		end
 
@@ -134,5 +134,4 @@ RSpec.describe AddressBook do
        expect(entry).to be_nil
     end
 	end
-
 end
